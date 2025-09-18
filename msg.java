f06e8c2ba325b89a8f9b9f72c74ad2eb30265518
kowshika <gyalamkowshika123@gmail.com>
2025-09-18 11:49:32 +0530

    Revert "updated-code"
    This reverts commit 02bc032151e79b0933359a8974d0af5968862537.

--- msg.java
+++ msg.java
@@ -1,6 +1,4 @@
 public class msg{
 	public static void main(String[] args){
-		System.out.println("this is old one");
-	        System.out.println("the new one"); 
-	}
+		System.out.println("this is old one");}
 }
