02bc032151e79b0933359a8974d0af5968862537
kowshika <gyalamkowshika123@gmail.com>
2025-09-18 11:45:51 +0530

    updated-code

--- msg.java
+++ msg.java
@@ -1,4 +1,6 @@
 public class msg{
 	public static void main(String[] args){
-		System.out.println("this is old one");}
+		System.out.println("this is old one");
+	        System.out.println("the new one"); 
+	}
 }
